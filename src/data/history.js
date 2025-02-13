c5f051978b4053c2911156f98c36f834b401c26a
Vandana-jain-123 <vandana12345jain@gmail.com>
2025-02-14 01:46:58 +0530

    experiece updated

--- src/data/history.js
+++ src/data/history.js
@@ -5,27 +5,9 @@ const history=[
     "startDate": "April, 2024",
     "endDate": "August,2024",
     "experiences": ["Developed and optimized responsive web applications using React.js, JavaScript, HTML, CSS.","API Integrations", "Reduced load times by 50%"],
-    "imageSrc": "history/google.png"
+    "imageSrc": "history/visas.jpeg"
   },
-  // {
-  //   "role": "UI Designer",
-  //   "organisation": "Mirosoft",
-  //   "startDate": "Aug, 2021",
-  //   "endDate": "Aug, 2022",
-  //   "experiences": ["Worked on Windows 11", "Designed the control panel"],
-  //   "imageSrc": "history/microsoft.png"
-  // },
-  // {
-  //   "role": "SWE Intern",
-  //   "organisation": "Netflix",
-  //   "startDate": "Apr, 2020",
-  //   "endDate": "Jun, 2020",
-  //   "experiences": [
-  //     "Worked on component library",
-  //     "Helped create UI components"
-  //   ],
-  //   "imageSrc": "history/netflix.png"
-  // }
+
 ]
 
 export default history
